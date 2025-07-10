9f31671943bdf1f447192ae015a1d249d4d5fdd0
ranandasatria <rananda24@gmail.com>
2025-07-10 19:58:11 +0700

    refactor: save token to reducer after user login

--- src/redux/reducers/auth.js
+++ src/redux/reducers/auth.js
@@ -2,17 +2,21 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
   currentUser: null,
-}
+  token: null,
+};
 
 const auth = createSlice({
   name: 'auth',
   initialState,
   reducers: {
     loginAction: (state, action) => {
-      state.currentUser = { ...action.payload }
+      const { user, token } = action.payload;
+      state.currentUser = user;
+      state.token = token;
     },
     logoutAction: (state) => {
       state.currentUser = null;
+      state.token = null;
     },
   },
 });
